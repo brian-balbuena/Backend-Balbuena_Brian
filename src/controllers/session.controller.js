33e4f2bb297e2a04c4ed3c241b0fc623762d9552
Brian Balbuena <brian.sir93@gmail.com>
2024-05-30 21:12:22 -0300

    prueba fetch0.6

--- src/controllers/session.controller.js
+++ src/controllers/session.controller.js
@@ -53,7 +53,7 @@ export const logoutSession = async (req, res) => {
         await serviceUser.updateLastConection(req.user.email);
 
 
-        res.send({ redirect: `http://localhost:${port}/login` });
+        res.send({ redirect: `${domain}/login` });
 
     } catch (error) {
         res.status(400).send({ error });
